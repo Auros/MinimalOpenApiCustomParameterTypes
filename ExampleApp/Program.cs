da7ac225d25752113c6c155db289b3c0ef81060d
Auros Nexus <41306347+Auros@users.noreply.github.com>
2024-07-27 18:39:37 -0400

    Add route constraint for verbosity
    (Even though it's not necessary)

--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -1,6 +1,9 @@
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddRouting(options => options.ConstraintMap.Add("ulid", typeof(UlidRouteConstraint)));
 
 var app = builder.Build();
 
@@ -11,9 +14,21 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/hello-world/{id}", (Ulid id) => 
+app.MapGet("/hello-world/{id:ulid}", (Ulid id) => 
 {
     return Results.Ok(new { Id = id }); 
 });
 
 app.Run();
+
+class UlidRouteConstraint : IRouteConstraint
+{
+    public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+    {
+        if (!values.TryGetValue(routeKey, out var routeValue))
+            return false;
+    
+        var value = Convert.ToString(routeValue, CultureInfo.InvariantCulture);
+        return value is not null && Ulid.TryParse(value, out _);
+    }
+}
